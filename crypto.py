53b4e74f5ccf6fe32fc5efb0f3913d1dd3240484
lopz82 <jorge.lopez.guzman@outlook.es>
2020-11-28 16:10:13 +0100

    Removing unnecessary bytes calls

--- crypto.py
+++ crypto.py
@@ -2,12 +2,12 @@ import bcrypt
 
 
 def hash_password(password: str, encoding: str = "utf-8") -> str:
-    encoded = bytes(password.encode(encoding))
+    encoded = password.encode(encoding)
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
     return hashed.decode(encoding)
 
 
 def is_correct_password(password: str, hashed: str, encoding: str = "utf-8") -> bool:
-    encoded_passwd = bytes(password.encode(encoding))
-    encoded_hash = bytes(hashed.encode(encoding))
+    encoded_passwd = password.encode(encoding)
+    encoded_hash = hashed.encode(encoding)
     return bcrypt.checkpw(encoded_passwd, encoded_hash)
